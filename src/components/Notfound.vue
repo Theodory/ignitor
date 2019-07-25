388478dc130b4aaadd996e07932a1575feae31d8
Theodory <theodoryf@gmail.com>
2019-07-25 20:17:26 +0300

    Add 404 page

---
+++ src/components/Notfound.vue
@@ -0,0 +1,18 @@
+<template>
+	<b-container>  
+		<b-row class="justify-content-md-center">
+			<b-col align-self="center">
+				<div>
+					<b-jumbotron header="Page Not Found" lead="">
+						<b-button variant="primary" href="#">Back</b-button>
+					</b-jumbotron>
+				</div>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>		
+<script>
+export default {
+	name: 'Notfound'
+}
+</script>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue"
 import VueRouter from 'vue-router'
 import Home from './components/Home.vue'
 import Post from './components/PostList.vue'
+import NotFound from './components/Notfound.vue'
 Vue.use(VueRouter)
 
 export default new VueRouter({
@@ -9,6 +10,11 @@ export default new VueRouter({
 	base: '/',
 	linkActiveClass: "active",
 	routes: [
+	{
+	  // will match everything
+	  path: '*',
+	  component: NotFound
+	},
 	{
 		path: "/",
 		name: "Home",
