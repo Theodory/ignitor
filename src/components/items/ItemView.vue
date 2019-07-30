b248b9c8b6c8e375c5d6ab54c6f89b000007a4b3
Theodory <theodoryf@gmail.com>
2019-07-30 18:45:10 +0300

    Add back button

--- src/components/items/ItemView.vue
+++ src/components/items/ItemView.vue
@@ -5,7 +5,7 @@
 		</b-row>
 		<b-row v-if="!loading">
 			<b-col>
-				<h1>{{ post.title }}</h1>
+				<h1><button @click="back()">Back</button>{{ post.title }}</h1>
 				<h4 class="text-muted">
 				</h4>
 				<hr />
@@ -50,6 +50,9 @@
 			},
 			created() {
 				this.fetchItem();
+			},
+			back(){
+				this.$router.push('/')
 			}
 		}
 	}
